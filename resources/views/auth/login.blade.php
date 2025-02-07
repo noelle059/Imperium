f30841f88286633883419a73055fdabc8a72cf9e
xxnieljohn <gonzaloseanruzzel@gmail.com>
2025-02-07 17:14:00 +0800

    ui change

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,9 +39,11 @@
                     </p>
                     <div class="flex items-center justify-end mt-4">
     @if (Route::has('password.request'))
-        <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-            {{ __('Forgot your password?') }}
-        </a>
+        <div class="forgot-password">
+            <a href="{{ route('password.request') }}">
+                {{ __('Forgot your password?') }}
+            </a>
+        </div>
     @endif
 </div>
                 </form>
@@ -89,7 +91,7 @@
                         </p>
                 </form>
             </div>
-            <div class="imgBx"><img src="images/SIGNUP_ICON.png" alt="" /></div>
+            <div class="imgBx"><img src="images/CSD.png" alt="" /></div>
         </div>
     </div>
 
