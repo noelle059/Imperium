5b3bf70a7f822e947e48a8e0f628c7125c4ab77e
xxnieljohn <gonzaloseanruzzel@gmail.com>
2025-02-21 00:22:54 +0800

    with about & footer

---
+++ public/scripts/feedback.js
@@ -0,0 +1,58 @@
+let track = document.querySelector('.testimonial-track');
+let prevBtn = document.querySelector('.prev-btn');
+let nextBtn = document.querySelector('.next-btn');
+let cardWidth = document.querySelector('.testimonial-card').offsetWidth + 20;
+let totalCards = track.children.length;
+let index = 0;
+let visibleCards;
+
+// Limit the number of testimonial cards to 10
+if (totalCards > 10) {
+    while (track.children.length > 10) {
+        track.removeChild(track.lastElementChild);
+    }
+    totalCards = 10; // Update totalCards after removing extra ones
+}
+
+function updateVisibleCards() {
+    let wrapperWidth = document.querySelector('.testimonial-track-wrapper').offsetWidth;
+    
+    // Show only 1 card per slide on smaller screens
+    visibleCards = window.innerWidth < 768 ? 1 : Math.floor(wrapperWidth / cardWidth);
+    
+    track.style.transform = `translateX(-${index * cardWidth}px)`;
+    updateButtons();
+}
+
+// Update on resize
+window.addEventListener('resize', () => {
+    cardWidth = document.querySelector('.testimonial-card').offsetWidth + 20;
+    updateVisibleCards();
+});
+
+nextBtn.addEventListener('click', () => {
+    if (index < totalCards - visibleCards) { 
+        index++;
+        track.style.transform = `translateX(-${index * cardWidth}px)`;
+    }
+    updateButtons();
+});
+
+prevBtn.addEventListener('click', () => {
+    if (index > 0) {
+        index--;
+        track.style.transform = `translateX(-${index * cardWidth}px)`;
+    }
+    updateButtons();
+});
+
+function updateButtons() {
+    prevBtn.style.opacity = index === 0 ? "0.5" : "1"; 
+    prevBtn.style.pointerEvents = index === 0 ? "none" : "auto";
+
+    nextBtn.style.opacity = index >= totalCards - visibleCards ? "0.5" : "1"; 
+    nextBtn.style.pointerEvents = index >= totalCards - visibleCards ? "none" : "auto";
+}
+
+// Initial setup
+updateVisibleCards();
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,31 +4,41 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>Homepage</title>
     <link rel="icon" type="image/svg" href="{{ asset('images/FAVICON_1.png') }}">
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     <link rel="stylesheet" href="{{ asset('css/about.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/faq.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/feedback.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
     <link rel="stylesheet" href="{{ asset('css/mediaquery.css') }}">
+    <script src="{{ asset('scripts/feedback.js') }}" defer></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    
     <!--ABOUT-->
     <link rel="stylesheet" href="https://unpkg.com/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://unpkg.com/bs-brain@2.0.4/components/abouts/about-2/assets/css/about-2.css">   
     <!--ABOUT-->
-    <!--FAQ-->
-    <link href="css/owl.carousel.min.css" rel="stylesheet" type="text/css" />
-    <script src="js/jquery-3.4.1.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <!--FAQ-->
+
+    <!--FEEDBACK-->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <!--FEEDBACK-->
+    
+    <!-- FOOTER -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css'>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css'>
+    <link href='//netdna.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' rel='stylesheet'/>
+    <!-- FOOTER -->
 </head>
 
 <body>
     @include('class.navbar')
     @include('class.carousel')
     @include('class.about')
-    @include('class.faq')
-
+    @include('class.feedback')
+    @include('class.footer')
 
 
 
