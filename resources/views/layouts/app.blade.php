46e8ec8e8a8df5fc56b21418e990e8bf961a291a
Vincent Davac <davacvincent.bsit@gmail.com>
2025-02-21 13:52:25 +0800

    bootstrap locally

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,7 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+            <!-- Navbar -->
             @include('layouts.navigation')
 
             <!-- Page Heading -->
@@ -32,7 +33,12 @@
 
             <!-- Page Content -->
             <main>
+
+
             @yield('content')
+
+
+
             </main>
         </div>
     </body>
