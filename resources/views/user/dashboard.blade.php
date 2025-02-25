4fdc1b1d0b8398b488193a8e56612d6cb484e83c
xxnieljohn <gonzaloseanruzzel@gmail.com>
2025-02-25 21:10:13 +0800

    ok na to ulit

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -7,6 +7,7 @@
     <title>Dashoard</title>
     <link rel="icon" type="image/svg" href="{{ asset('images/FAVICON_1.png') }}">
     <link rel="stylesheet" href="/bootstrap-5.3.3-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="{{ asset('css/navigation.css') }}">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
